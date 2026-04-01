a25a2d5a7c35c00e4121b99623dbea760096b37c
sinan <sinan@bpract.com>
2026-04-01 14:36:42 +0530

    fix: inject style issue

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'tsup';
+import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ['src/index.ts'],
-  format: ['cjs', 'esm'],
+  entry: ["src/index.ts"],
+  format: ["cjs", "esm"],
   dts: true,
   clean: true,
-  external: ['react', 'react-dom'],
+  external: ["react", "react-dom"],
   sourcemap: true,
   minify: true,
-  injectStyle: true,
+  injectStyle: false,
 });
